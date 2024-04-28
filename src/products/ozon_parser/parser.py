ea691cfbb48718a2cdbce50e152e78c3ea2f9000
bulatsabirow <bulatsabirow@gmail.com>
2024-04-28 11:59:48 +0300

    = bypass cloudflare

--- src/products/ozon_parser/parser.py
+++ src/products/ozon_parser/parser.py
@@ -1,9 +1,11 @@
 import os
 import time
+from concurrent import futures
 from concurrent.futures import ThreadPoolExecutor
 from pprint import pprint
 from urllib.parse import urlencode
 
+from products.models import Product
 from products.ozon_parser.scraper import ProductLinksScraper, ProductScraper
 
 
@@ -17,20 +19,19 @@ class OzonProductParser:
         ).get_text()
         price = response.find("span", {"class": "x3l lx4 x7l"}).get_text()
         description = response.select_one("#section-description .RA-a1").get_text()
-        image_url = response.select_one(".j8v vj8 > .v8j .jv9 .b900-a").get("src")
-        name = response.find("h1", {"class": ".yl .tsHeadline550Medium"}).get_text()
-        pprint(
-            {
-                "name": name,
-                "description": description,
-                "image_url": image_url,
-                "price": price,
-                "discount": discount,
-            }
-        )
+        image_url = response.select_one(".j8v.vj8 > .v8j.jv9.b900-a").get("src")
+        name = response.find("h1", {"class": "yl tsHeadline550Medium"}).get_text()
+        return {
+            "name": name,
+            "description": description,
+            "image_url": image_url,
+            "price": price,
+            "discount": discount,
+        }
 
 
 class OzonParser:
+    base_url = "https://www.ozon.ru"
     links_scraper = ProductLinksScraper()
     product_parser = OzonProductParser()
 
@@ -47,16 +48,31 @@ class OzonParser:
             )
             self.products_count -= self.links_scraper.PRODUCTS_PER_PAGE
             product_links.extend(
-                element.get("href") for element in response.css.select(".iy5 > a")
+                self.base_url + element.get("href")
+                for element in response.css.select(".iy5 > a")
             )
             query_params["page"] += 3
 
         product_links = product_links[: self.products_count]
-
         with ThreadPoolExecutor(max_workers=2) as executor:
-            executor.map(self.product_parser.parse, product_links)
+            tasks = [
+                executor.submit(self.product_parser.parse, product_link)
+                for product_link in product_links
+            ]
+            products = []
+            for finished_task in futures.as_completed(tasks):
+                # TODO migrations
+                products.append(Product(**finished_task.result()))
+
+        return products
 
 
 if __name__ == "__main__":
-    ozon = OzonParser(50)
-    ozon.parse()
+    # ozon = OzonParser(50)
+    # ozon.parse()
+    p = OzonProductParser()
+    print(
+        p.parse(
+            "https://www.ozon.ru/product/ersh-dlya-unitaza-ersh-ersh-dlya-unitaza-napolnyy-ershiki-dlya-unitaza-ershik-dlya-unitaza-147121868/?_bctx=CAQQAQ&asb=DEgxhPQmIcDRO5aZ0CjN7k98R%252FPqJZXjYY1JjerGCDw%253D&asb2=ef3Q89SS-nwdribIpyMJFxt98dXLz1lcy1jq60NTHhG6ZfKb4BsNz9Zs2bcvkrYr&avtc=1&avte=4&avts=1714253615&hs=1"
+        )
+    )
--- src/products/ozon_parser/scraper.py
+++ src/products/ozon_parser/scraper.py
@@ -1,19 +1,21 @@
+import time
+
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from seleniumbase import Driver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
-
-from products.ozon_parser.driver import BypassingAntibotChallengeChrome
 
 
 class BaseScraper:
     PAGE_LOADING_TIMEOUT = 15
 
     def __init__(self):
-        self.driver = BypassingAntibotChallengeChrome(
-            service=Service(ChromeDriverManager().install())
+        self.driver = Driver(
+            uc=True,
+            headed=True,
         )
 
     def scrap(self, url):
@@ -21,29 +23,30 @@ class BaseScraper:
 
 
 class ProductLinksScraper(BaseScraper):
-    URL = "https://www.ozon.ru/seller/1/products/?"
+    URL = "https://www.ozon.ru/seller/proffi-1/products/?miniapp=seller_1&"
     PRODUCTS_PER_PAGE = 36
 
-    def scrap(self, url=""):
+    def scrap(self, url=URL):
         method = (
             lambda driver: len(driver.find_elements(By.CSS_SELECTOR, ".iy5 > a"))
             == self.PRODUCTS_PER_PAGE
         )
-
-        self.driver.get(self.URL)
-        # Wait until all products' cards will be loaded
-        WebDriverWait(self.driver, timeout=self.PAGE_LOADING_TIMEOUT).until(method)
-        soup = BeautifulSoup(self.driver.page_source, "lxml")
-        return soup
+        with self.driver:
+            self.driver.get(url)
+            # Wait until all products' cards will be loaded
+            WebDriverWait(self.driver, timeout=self.PAGE_LOADING_TIMEOUT).until(method)
+            soup = BeautifulSoup(self.driver.page_source, "lxml")
+            return soup
 
 
 class ProductScraper(BaseScraper):
     def scrap(self, url):
-        self.driver.get(url)
-        WebDriverWait(self.driver, timeout=self.PAGE_LOADING_TIMEOUT).until(
-            EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "#section" "-description > div.wk6 > h2")
+        with self.driver:
+            self.driver.get(url)
+            WebDriverWait(self.driver, timeout=self.PAGE_LOADING_TIMEOUT).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, "#section-description > div.wk6 > h2")
+                )
             )
-        )
-        soup = BeautifulSoup(self.driver.page_source, "lxml")
-        return soup
+            soup = BeautifulSoup(self.driver.page_source, "lxml")
+            return soup
